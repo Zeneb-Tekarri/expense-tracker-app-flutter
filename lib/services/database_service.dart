a1336669ed030a4e38f32fe4bf04ba434e3f08d8
Zeneb-Tekarri <zeinebtekarri@gmail.com>
2026-06-10 14:50:18 +0200

    Implement SQLite CRUD Operations

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_app/models/transaction.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -8,6 +9,7 @@ class DatabaseService {
     _database = await _initDatabase();
     return _database!;
   }
+  
   Future<Database> _initDatabase() async {
     final path = join(await getDatabasesPath(), 'expense_tracker.db');
 
@@ -16,7 +18,7 @@ class DatabaseService {
       version: 1,
       onCreate: (db, version) async {
         await db.execute('''
-          CREATE TABLE expenses (
+          CREATE TABLE transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             amount REAL NOT NULL,
@@ -24,6 +26,44 @@ class DatabaseService {
           )
         ''');
       },
+    );
+  }
+
+  Future<int> insertTransaction(TransactionModel transaction,) async {
+    final db = await database;
+
+    return await db.insert(
+      'transactions',
+      transaction.toMap(),
+    );
+  }
+
+  Future<List<TransactionModel>> getTransactions()  async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps =  await db.query('transactions');
+
+    return List.generate(maps.length,(index) => TransactionModel.fromMap(
+      maps[index], ),);
+  }
+
+  Future<int> updateTransaction(TransactionModel transaction,) async {
+    final db = await database;
+
+    return await db.update(
+      'transactions',
+      transaction.toMap(),
+      where: 'id = ?',
+      whereArgs: [transaction.id],
+    );
+  }
+
+  Future<int> deleteTransaction(int id,) async {
+    final db = await database;
+
+    return await db.delete(
+      'transactions',
+      where: 'id = ?',
+      whereArgs: [id],
     );
   }
 }
